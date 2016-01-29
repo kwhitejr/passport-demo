1d80242132cc9f447fe8ddbe7b0da351c441b518
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-28 19:10:55 -1000

    added jade and created login form, secret page

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express');
 
 var CONFIG = require('./config');
-console.log(CONFIG);
 
 var app = express();
 
+app.set('views', 'views');
+app.set('view engine', 'jade');
 
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.post('/login', function (req, res) {
+  //TODO: implement login
+  res.send('logging in...');
+});
+
+app.get('/secret', function (req, res) {
+  res.render('secret');
+});
 
 var server = app.listen(CONFIG.PORT, function () {
   console.log('listening on port ' + CONFIG.PORT);
 });
+
+// If you want to add logic to the config file, then you must convert it to module.exports object.
